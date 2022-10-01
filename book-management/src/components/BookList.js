c351ba2f939f2e1e50c8247fddf7b57641ff1c58
koechcoding <koechkelvin2001@gmail.com>
2022-10-01 17:12:19 +0300

    Initial commit

---
+++ book-management/src/components/BookList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BookList=()=>{
+    return <h2>List of books</h2>;
+};
+
+export default BookList;
---
+++ book-management/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './router/appRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss';
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
